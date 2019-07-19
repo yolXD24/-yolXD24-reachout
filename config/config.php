4356b4f45b0be17d1bf0969c69db104f0ae99733
yolXD24 <52404008+yolXD24@users.noreply.github.com>
2019-07-19 15:30:40 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'pnpassword');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'xVMI2W19dq');
+define('DB_PASSWORD', 'HT6LyFpIV8');
 define('DB_NAME', 'hackathonV2');
  
 /* Attempt to connect to MySQL database */
