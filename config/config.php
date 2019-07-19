8a81eb4bc4ee0812b26e1baae35a360252c1265a
yolXD24 <52404008+yolXD24@users.noreply.github.com>
2019-07-19 15:52:11 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors',1);
-define('DB_SERVER', 'remotemysql.com:3306');
+define('DB_SERVER', 'remotemysql.com');
 define('DB_USERNAME', 'xVMI2W19dq');
 define('DB_PASSWORD', 'HT6LyFpIV8');
 define('DB_NAME', 'xVMI2W19dq');
@@ -10,7 +10,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME,3306);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error().'\n'.DB_SERVER.'\n'.DB_NAME);
+    die("ERROR: Could not connect. " . mysqli_connect_error().'Yol');
 }else{
 
 }
