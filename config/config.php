3ddddd9800f588521a3600b906210d774578ab22
yolXD24 <52404008+yolXD24@users.noreply.github.com>
2019-07-19 15:41:28 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error().'\n'.DB_SERVER.'\n'.DB_NAME);
 }else{
 
 }
